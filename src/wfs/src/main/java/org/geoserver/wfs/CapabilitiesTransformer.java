0e3d5567c80486bc9a4fdf45dd638cb72de45ca8
Jesse Eichar <jesse.eichar@camptocamp.com>
2012-08-20 16:22:41 +0200

    fixed compile error

--- src/wfs/src/main/java/org/geoserver/wfs/CapabilitiesTransformer.java
+++ src/wfs/src/main/java/org/geoserver/wfs/CapabilitiesTransformer.java
@@ -16,6 +16,7 @@ import java.util.Arrays;
 import java.util.Collection;
 import java.util.Collections;
 import java.util.Comparator;
+import java.util.Enumeration;
 import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
